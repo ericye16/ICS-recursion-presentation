dd4347e73809c35c392ead7d2cf2a59247e75bcd
Eric Ye <ericye16@gmail.com>
2013-09-15 18:46:18 -0400

    And now as an enum. This might work better.

--- src/Cities.java
+++ src/Cities.java
@@ -1,60 +1,113 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.HashMap;
 
-/**
- * User: eric
- * Date: 15/09/13
- * Time: 5:29 PM
- */
-public class Cities {
-
-    public int indx;
-    public static String[] cityNames = {"Home","Cornell", "Harvard", "Yale", "Boston", "Falls"};
-
-    private HashMap<TwoCities,Integer> distances;
-
-    class TwoCities {
-        public Cities cityOne;
-        public Cities cityTwo;
-        public TwoCities(Cities one, Cities two) {
-            cityOne = one;
-            cityTwo = two;
-        }
-        public int hashCode() {
-            return 0x1 << cityOne.indx & 0x1 << cityTwo.indx; // goodness I hope this works
-        }
-        public boolean equals(Object obj) {
-            if (obj instanceof TwoCities && ((TwoCities) obj).cityOne == this.cityOne &&
-                    ((TwoCities) obj).cityTwo == this.cityTwo) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-    }
-
-    public Cities(int cityId) {
-        indx = cityId;
-        for (int i = 0; i < cityNames.length; i++) {
-            if (i == cityId) continue;
-
-        }
-    }
-
-    public static void initializeMap() {
-        for (int i = 0; i < cityNames.length; i++) {
-            for (int j = 1; j < i; j++) { // quadratic loop
-                switch (i) {
-                    case 0:
-                }
-            }
-        }
-    }
+public enum Cities {
+    Home,
+    Cornell,
+    Harvard,
+    Yale,
+    Boston,
+    Falls;
 
     public int distance(Cities other) {
-        //TODO: implement
-        return 0;
+        int dist = -1;
+        switch (this) {
+            case Home:
+                switch (other) {
+                    case Home:
+                        dist = 0;
+                        break;
+                    case Cornell:
+                        dist = 397;
+                        break;
+                    case Harvard:
+                        dist = 881;
+                        break;
+                    case Yale:
+                        dist = 820;
+                        break;
+                    case Boston:
+                        dist = 886;
+                        break;
+                    case Falls:
+                        dist = 338;
+                        break;
+                }
+                break;
+            case Cornell:
+                switch (other) {
+                    case Cornell:
+                        dist = 0;
+                        break;
+                    case Harvard:
+                        dist = 530;
+                        break;
+                    case Yale:
+                        dist = 419;
+                        break;
+                    case Boston:
+                        dist = 533;
+                        break;
+                    case Falls:
+                        dist = 67;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case Harvard:
+                switch (other) {
+                    case Harvard:
+                        dist = 0;
+                        break;
+                    case Yale:
+                        dist = 217;
+                        break;
+                    case Boston:
+                        dist = 5;
+                        break;
+                    case Falls:
+                        dist = 557;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case Yale:
+                switch (other) {
+                    case Yale:
+                        dist = 0;
+                        break;
+                    case Boston:
+                        dist = 222;
+                        break;
+                    case Falls:
+                        dist = 502;
+                        break;
+                }
+                break;
+            case Boston:
+                switch (other) {
+                    case Boston:
+                        dist = 0;
+                        break;
+                    case Falls:
+                        dist = 502;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case Falls:
+                switch (other) {
+                    case Falls:
+                        dist = 0;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
+        if (dist == -1) return other.distance(this);
+        else return dist;
     }
 }
