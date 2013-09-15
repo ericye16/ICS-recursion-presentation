61c5c564ea1dc381f70fcb1923729ae6686e3772
Eric Ye <ericye16@gmail.com>
2013-09-15 16:44:01 -0400

    Added main class

---
+++ src/ProgramMain.java
@@ -0,0 +1,13 @@
+/**
+ * The main class for this project
+ * Created with IntelliJ IDEA.
+ * User: eric
+ * Date: 15/09/13
+ * Time: 4:23 PM
+ */
+public class ProgramMain {
+    public static void main(String[] args) {
+
+    }
+
+}
