df14531c865415fd18f0a47ae0c4a1b27eda900b
Eric Ye <ericye16@gmail.com>
2013-09-15 17:36:17 -0400

    Added cities class

---
+++ src/Cities.java
@@ -0,0 +1,18 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * User: eric
+ * Date: 15/09/13
+ * Time: 5:29 PM
+ */
+public class Cities {
+
+    public Cities(int cityId) {
+
+    }
+
+    public int distance(Cities other) {
+        //TODO: implement
+        return 0;
+    }
+}
