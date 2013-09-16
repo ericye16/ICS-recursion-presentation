31094c32ab5f60519e981f50930aafd9f7ef4823
Yaning Tan <yaningtan@hotmail.com>
2013-09-15 20:27:25 -0400

    This isn't much. Honestly, I have no idea what I'm doing.

--- src/ProgramMain.java
+++ src/ProgramMain.java
@@ -5,9 +5,43 @@
  * Date: 15/09/13
  * Time: 4:23 PM
  */
+
+import java.util.*;
+import java.lang.String;
+
 public class ProgramMain {
+
+    public static int HeldKarpEqn ( HashSet S , int el){
+        if (S.size() == 1){
+            return cityA.distance(cityB);   //from start to city L, change vars later
+        }
+        else if (S.size>1){
+
+        }
+    }
+
+    public static void HeldKarpDriver (){
+        HashSet subCities = new HashSet();
+        subCities = ();
+
+        int minC;
+        int newC;
+        int el;
+        int i;
+
+        minC = 10000000;
+
+        i = HeldKarpEqn(subCities, el);
+        newC = i + cityA.distance(cityB); //change cities later
+        if (newC<minC){
+            minC = newC;
+
+        }
+
+    }
+
+
     public static void main(String[] args) {
-        System.out.print("Hello World");
     }
 
 }
